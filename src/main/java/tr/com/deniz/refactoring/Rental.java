dc73c198d6c2f321dbec72d20add7466bd8863bc
Deniz Salman <ddenizsalman@gmail.com>
2023-02-26 17:26:32 +0300

    Refactor Move Method, getCharge and getFrequentRenterPoints from Rental to Movie

--- src/main/java/tr/com/deniz/refactoring/Rental.java
+++ src/main/java/tr/com/deniz/refactoring/Rental.java
@@ -8,35 +8,13 @@ public class Rental {
         this.movie = movie;
         this.daysRented = daysRented;
     }
-
-    public double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR -> {
-                result += 2;
-                if (getDaysRented() > 2) {
-                    result += (getDaysRented() - 2) * 1.5;
-                }
-            }
-            case Movie.NEW_RELEASE -> result += getDaysRented() * 3;
-            case Movie.CHILDREN -> {
-                result += 1.5;
-                if (getDaysRented() > 3) {
-                    result += (getDaysRented() - 3) * 1.5;
-                }
-            }
-        }
-        return result;
-    }
-
     public int getFrequentRenterPoints() {
-        if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) {
-          return 2;
-        } else {
-            return 1;
-        }
+        return movie.getFrequentRenterPoints(daysRented);
     }
-    
+    public double getCharge() {
+        return movie.getCharge(daysRented);
+    }
+
     public Movie getMovie() {
         return movie;
     }
@@ -52,4 +30,5 @@ public class Rental {
     public void setDaysRented(int daysRented) {
         this.daysRented = daysRented;
     }
+
 }
